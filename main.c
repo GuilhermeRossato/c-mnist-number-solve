781a87bfee5462ff9724cc8e93ad067fbc910f9e
Guilherme Rossato <guilhermrossato@gmail.com>
2020-04-15 05:33:11 +0000

    Added code for XOR training

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 
 #define FANN_NO_DLL
 #define FANN_NO_SEED
+#define DISABLE_PARALLEL_FANN
 
 #include "fann/doublefann.c"
 #include "fann/include/fann.h"
@@ -42,7 +43,7 @@ struct hyperparameters * create_hyperparameters(int processId) {
     h->layers[3] = 1;
     h->layers[4] = 0;
     h->is_random_weight = 1;
-    h->training_algorithm = FANN_TRAIN_BATCH;
+    h->training_algorithm = TRAINING_ALGORITHM_INCREMENTAL;
     return h;
 }
 
@@ -89,12 +90,70 @@ int main() {
 
     struct hyperparameters * h = create_hyperparameters(0);
 
+    printf("Creating network.\n");
     struct fann * ann = create_network_with_hyperparameters(h);
 
     fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-    fann_print_connections(ann);
+    printf("Creating data.\n");
+    double ** input = malloc(sizeof(double*) * 4);
+    double ** output = malloc(sizeof(double*) * 4);
+    for (int i = 0; i < 4; i++) {
+        input[i] = malloc(sizeof(double) * 2);
+        output[i] = malloc(sizeof(double) * 1);
+        int x = i % 2;
+        int y = i / 2;
+
+        input[i][0] = x == 0 ? -1.0 : 1.0;
+        input[i][1] = y == 0 ? -1.0 : 1.0;
+        output[i][0] = x != y ? 1.0 : -1.0;
+    }
+
+    struct fann_train_data * data = fann_create_train_pointer_array(
+        4,
+        2,
+        input,
+        1,
+        output
+    );
+
+    if (rand()%2) {
+        printf("Training network on data struct:\n");
+
+        fann_train_on_data(
+            ann,
+            data,
+            50000,
+            5000,
+            0.001
+        );
+    } else {
+        printf("Training network on file 'xor.data':\n");
+
+        fann_train_on_file(
+            ann,
+            "xor.data",
+            50000, // max epochs
+            10000, // epochs between reports
+            0.001 // desired error
+        );
+    }
+
+    printf("Testing network:\n");
+    for (int i = 0; i < 4; i++) {
+        double * result = fann_run(ann, input[i]);
+        double * expected = output[i];
+        printf("%5.1f, %5.1f = %6.2f (expected %6.2f, error: %6.2f)\n", input[i][0], input[i][1], result[0], expected[0], expected[0] - result[0]);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        free(input[i]);
+        free(output[i]);
+    }
+    free(input);
+    free(output);
+    fann_destroy_train(data);
 
     fann_destroy(ann);
     destroy_hyperparameters(h);
